d52887ab9c739833ff9ae8c037dcc2ebebdc0d63
mcMineyC <jedi.master@falconhosting.net>
2024-07-03 12:55:54 -0700

    Initial commit

---
+++ bin/ytproxy.dart
@@ -0,0 +1,33 @@
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+
+void main(List<String> arguments) async {
+  YoutubeExplode yt = YoutubeExplode();
+  var app = Router();
+
+  app.get('/hello', (Request request) {
+    return Response.ok('hello-world');
+  });
+
+  app.get('/url/<id>', (Request request, String id) async {
+    // print('url id: $id');
+    var media = await yt.videos.streamsClient.getManifest(id);
+    var streamInfo = media.audioOnly.withHighestBitrate();
+    // print('url: ${streamInfo.url.toString()}');
+    return Response.ok(streamInfo.url.toString());
+  });
+  var handler = const Pipeline()
+    .addMiddleware(logRequests())
+    .addMiddleware(corsHeaders(originChecker: (origin) {
+      if(origin.contains('localhost')) return true;
+      else if(origin.contains('eatthecow.mooo.com')) return true;
+      else if(origin.contains('taxi-native.vercel.app')) return true;
+      else return false;
+    }))
+    .addHandler(app);
+  var server = await io.serve(handler, 'localhost', 8080);
+  print("Server running at http://${server.address.host}:${server.port}");
+}
---
+++ test/ytproxy_test.dart
@@ -0,0 +1,8 @@
+import 'package:ytproxy/ytproxy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
